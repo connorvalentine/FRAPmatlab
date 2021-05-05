06949d41f8696eac0ae6e7025651122fc663a74e
connorvalentine <connorv@andrew.cmu.edu>
2021-05-05 12:43:24 -0400

    working multiposition tracking

--- fun_radius_finder_0.m
+++ fun_radius_finder_0.m
@@ -199,7 +199,7 @@ for field = fieldnames(id)' % iterate through the position list in id structure
     fits.(position).('I_t1') = I_t1;
     fits.(position).('ref_1') = ref_1;
     fits.(position).('radius') = FWHM/2;
-    fits.(position).('err_radius') = FWHM_err;
+    fits.(position).('err_radius') = FWHM_err/2;
     
     if counter == npoints
         counter = 0;
